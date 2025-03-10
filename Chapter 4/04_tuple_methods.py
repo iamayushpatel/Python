9fadcbbea4779a9abb7a8fd37a281013120a9aec
Ayush Patel <iamayushpatelap@gmail.com>
2025-03-10 21:13:12 +0530

    Ch-4

---
+++ Chapter 4/04_tuple_methods.py
@@ -0,0 +1,13 @@
+a = (1,45,342,3424,False, 45, "Rohan", "Shivam")
+print(a) 
+
+no = a.count(45)
+print(no)
+
+i = a.index(3424)
+print(i)
+
+print(len(a))
+d = {1 : "Pyhton" ,2 : [1,2,3]}
+d[2].append(4)
+print(d)
